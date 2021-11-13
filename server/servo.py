79ef392a3ee896cde333ea88fda41ad7d0e3e282
markovg <markovg@robots.com>
2021-11-13 12:42:53 -0500

    intermediate

--- server/servo.py
+++ server/servo.py
@@ -10,6 +10,25 @@ import sys
 import Adafruit_PCA9685
 import ultra
 
+from pydub import AudioSegment
+from pydub.playback import play
+
+bell = AudioSegment.from_wav("media/bell_short.wav")
+
+import sounddevice as sd
+import soundfile as sf
+
+filename = 'myfile.wav'
+# Extract data and sampling rate from file
+bell = sf.read("media/bell_short.wav", dtype='float32')
+def play_sound(data, fs):
+        sd.play(data,fs, blocking=True)
+
+from threading import Thread
+def play_bell():
+        T = Thread(target=play_sound,args=bell)
+        T.start()
+
 '''
 change this form 1 to 0 to reverse servos
 '''
@@ -23,18 +42,18 @@ pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(50)
 
 pwm0_init = 300
-pwm0_max  = 450
-pwm0_min  = 150
+pwm0_max  = 500
+pwm0_min  = 100
 pwm0_pos  = pwm0_init
 
-pwm1_init = 300
+pwm1_init = 350
 pwm1_max  = 480
-pwm1_min  = 160
+pwm1_min  = 100
 pwm1_pos  = pwm1_init
 
-pwm2_init = 300
+pwm2_init = 350
 pwm2_max  = 500
-pwm2_min  = 100
+pwm2_min  = 250
 pwm2_pos  = pwm2_init
 
 pwm3_init = 300
@@ -44,7 +63,7 @@ pwm3_pos  = pwm3_init
 
 pwm4_init = 300
 pwm4_max  = 500
-pwm4_min  = 100
+pwm4_min  = 240
 pwm4_pos  = pwm4_init
 
 org_pos = 300
@@ -87,12 +106,53 @@ def radar_scan():
 def ctrl_range(raw, max_genout, min_genout):
         if raw > max_genout:
                 raw_output = max_genout
+                play_bell()
+                
         elif raw < min_genout:
                 raw_output = min_genout
+                play_bell()
         else:
                 raw_output = raw
         return int(raw_output)
 
+def ctrl_range_arm(delta_hand, delta_arm, delta_look):
+        global pwm0_pos, pwm1_pos, pwm2_pos
+
+        if delta_look!=0:
+                if pwm2_pos<430 and pwm1_pos<125:
+                        pwm0_pos = ctrl_range(pwm0_pos, pwm0_init+50, pwm0_init-50)
+                elif pwm2_pos<380 and pwm1_pos<160:
+                        pwm0_pos = ctrl_range(pwm0_pos, pwm0_init+50, pwm0_init-50)
+                elif pwm2_pos<350 and pwm1_pos<180:
+                        pwm0_pos = ctrl_range(pwm0_pos, pwm0_init+50, pwm0_init-50)
+                else:
+                        pwm0_pos = ctrl_range(pwm0_pos, pwm0_max, pwm0_min)
+
+        if delta_hand!=0:
+                # if not facing front, handfown limits are more restrictive
+                if pwm0_pos>350 or pwm0_pos<250:
+                        if pwm1_pos<125:
+                                pwm2_pos = ctrl_range(pwm2_pos, pwm2_max, 430)
+                        elif pwm1_pos<160:
+                                pwm2_pos = ctrl_range(pwm2_pos, pwm2_max, 380)
+                        elif pwm1_pos<180:
+                                pwm2_pos = ctrl_range(pwm2_pos, pwm2_max, 350)
+                        
+                elif pwm1_pos<125:
+                        pwm2_pos = ctrl_range(pwm2_pos, pwm2_max, 430)
+                elif pwm1_pos<160:
+                        pwm2_pos = ctrl_range(pwm2_pos, pwm2_max, 380)
+                elif pwm1_pos<180:
+                        pwm2_pos = ctrl_range(pwm2_pos, pwm2_max, 350)
+
+        if delta_arm!=0:
+                if pwm2_pos<380:
+                        pwm1_pos = ctrl_range(pwm1_pos, pwm1_max, 160)
+                elif 
+                        
+                
+                        
+                        
 
 def camera_ang(direction, ang):
         global org_pos
@@ -126,7 +186,8 @@ def lookleft(speed):
                 pwm0_pos += speed
         else:
                 pwm0_pos -= speed
-        pwm0_pos = ctrl_range(pwm0_pos, pwm0_max, pwm0_min)
+        #pwm0_pos = ctrl_range(pwm0_pos, pwm0_max, pwm0_min)
+        ctrl_range_arm(0,0,1)
         pwm.set_pwm(0, 0, pwm0_pos)
 
 
@@ -135,8 +196,9 @@ def lookright(speed):
         if pwm0_direction:
                 pwm0_pos -= speed
         else:
-                pwm0_pos += speed
-        pwm0_pos = ctrl_range(pwm0_pos, pwm0_max, pwm0_min)
+                pwm0_pos += speed       
+        #pwm0_pos = ctrl_range(pwm0_pos, pwm0_max, pwm0_min)
+        ctrl_range_arm(0,0,1)
         pwm.set_pwm(0, 0, pwm0_pos)
 
 
@@ -146,7 +208,8 @@ def up(speed):
                 pwm1_pos -= speed
         else:
                 pwm1_pos += speed
-        pwm1_pos = ctrl_range(pwm1_pos, pwm1_max, pwm1_min)
+        #pwm1_pos = ctrl_range(pwm1_pos, pwm1_max, pwm1_min)
+        ctrl_range_arm(0,1,0)
         pwm.set_pwm(1, 0, pwm1_pos)
 
 
@@ -156,7 +219,8 @@ def down(speed):
                 pwm1_pos += speed
         else:
                 pwm1_pos -= speed
-        pwm1_pos = ctrl_range(pwm1_pos, pwm1_max, pwm1_min)
+        #pwm1_pos = ctrl_range(pwm1_pos, pwm1_max, pwm1_min)
+        ctrl_range_arm(0,1,0)
         pwm.set_pwm(1, 0, pwm1_pos)
 
 def handUp(speed):
@@ -222,9 +286,10 @@ def loose(speed):
 
 
 def servo_init():
+        pwm.set_pwm(2, 0, pwm2_pos)
+        time.sleep(1)
         pwm.set_pwm(0, 0, pwm0_pos)
         pwm.set_pwm(1, 0, pwm1_pos)
-        pwm.set_pwm(2, 0, pwm2_pos)
         pwm.set_pwm(3, 0, pwm3_pos)
         pwm.set_pwm(4, 0, pwm4_pos)
 
@@ -249,7 +314,7 @@ def get_direction():
 
 
 if __name__ == '__main__':
-        while 1:
+        while 0:
                 for i in range(0,100):
                         pwm.set_pwm(0,0,(300+i))
                         time.sleep(0.05)
